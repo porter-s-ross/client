14091981f8b678cbebb470e612809e4a6b8113ea
rss <opqlns@pm.me>
2022-03-22 12:43:19 -0500

    init_commit

---
+++ src/components/ProductForm.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+export default () => {
+    const[title, setTitle] = useState("")
+    const[price, setPrice]= useState("")
+    const[description, setDescription] = useState("")
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/product/', {
+            title,
+            price,
+            description
+        })
+            .then(res=>console.log(res))
+            .catch(err=>console.log(err))
+    }
+
+    return(
+        <form onSubmit={onSubmitHandler}> 
+            <div>
+                <label>Title:</label>
+                <input type="text" onChange={(e)=>setTitle(e.target.value)}  />
+            </div>
+            <div>
+                <label>Price:</label>
+                <input type="text" onChange={(e)=>setPrice(e.target.value)}  />
+            </div>
+            <div>
+                <label>Description:</label>
+                <input type="text" onChange={(e)=>setDescription(e.target.value)}  />
+            </div>
+            <input type="submit" />
+        </form>
+    )
+}
